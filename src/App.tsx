0b01a544fedb672e231bb6a84107bb1d3cd6f159
limbs722 <limbs722@gmail.com>
2022-01-31 17:33:18 +0900

    ✨ 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { HomeIntro } from './pages';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/" component={HomeIntro} />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/homeIntro/HomeIntro.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const HomeIntro: React.FC = () => {
+  return <div></div>;
+};
+
+export default HomeIntro;
