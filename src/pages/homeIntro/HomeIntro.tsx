2dfdc611de02adc3844de113a086e24c57e36d53
limbs722 <limbs722@gmail.com>
2022-01-31 20:10:40 +0900

    🛠 eslint, prettier 설정 수정

--- src/pages/homeIntro/HomeIntro.tsx
+++ src/pages/homeIntro/HomeIntro.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-const HomeIntro: React.FC = () => {
-  return <div></div>;
-};
-
-export default HomeIntro;
+export default function HomeIntro() {
+  return <div />;
+}
